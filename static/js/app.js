c74efcd4de50b56a450ca8ee2f963408d7c8c4a7
Molly Sullivan <mchandler02@gmail.com>
2021-04-21 21:23:14 -0500

    demographic data available, need to add to to page

--- static/js/app.js
+++ static/js/app.js
@@ -1,41 +1,41 @@
 // console.log("app.js is loading");
 
 //Code based on instructor's code shared during office hours
-function drawBarGraph(sampleID){
+function drawBarGraph(sampleID) {
     // console.log(`drawBarGraph(${sampleID})`);
     d3.json("data/samples.json").then(data => {
         // console.log(data)
         var samples = data.samples;
-        var resultArray = samples.filter(s => s.id ==sampleID);
+        var resultArray = samples.filter(s => s.id == sampleID);
         // console.log(resultArray);
         var result = resultArray[0];
         // console.log(result);
         var otu_ids = result.otu_ids;
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
-        yticks = otu_ids.slice(0,10).map(otuID => `OTU${otuID}`).reverse(); //TBD
+        yticks = otu_ids.slice(0, 10).map(otuID => `OTU${otuID}`).reverse(); //TBD
         var barData = {
             x: sample_values.slice(0, 10).reverse(), //TBD
             y: yticks,
             type: "bar",
-            text: otu_labels.slice(0,10).reverse(),
+            text: otu_labels.slice(0, 10).reverse(),
             orientation: "h"
         }
         var barArray = [barData];
         var barLayout = {
             title: "Top 10 Bacteria Cultures Found",
-            margin:{t:30, l: 150}
+            margin: { t: 30, l: 150 }
         }
         Plotly.newPlot("bar", barArray, barLayout)
     })
 }
 
-function drawBubbleChart(sampleID){
+function drawBubbleChart(sampleID) {
     // console.log(`drawBubbleChart(${sampleID})`);
     d3.json("data/samples.json").then(data => {
-        console.log(data)
+        // console.log(data)
         var samples = data.samples;
-        var resultArray = samples.filter(s => s.id ==sampleID);
+        var resultArray = samples.filter(s => s.id == sampleID);
         // console.log(resultArray);
         var result = resultArray[0];
         // console.log(result);
@@ -43,7 +43,7 @@ function drawBubbleChart(sampleID){
         var otu_labels = result.otu_labels;
         var sample_values = result.sample_values;
         yticks = sample_values.map(sample_values => `Sample Value ${sample_values}`);
-        console.log(yticks)
+        // console.log(yticks)
         var trace1 = {
             x: otu_ids,
             y: sample_values,
@@ -54,23 +54,33 @@ function drawBubbleChart(sampleID){
             }
         }
         // console.log(trace1);
-        var data = [trace1];
-        var layout = {
+        var bubbleData = [trace1];
+        var bubbleLayout = {
             title: 'Sample Values vs OTU ID',
             showlegend: false,
             height: 600,
-            width:600
+            width: 1200
         };
-        Plotly.newPlot("bubble", data, layout);
-        })
-    }
-
-
-function showMetaData(sampleID){
-    // console.log(`showMetaData(${sampleID})`);
+        Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+    })
 }
 
-function optionChanged(newSampleID){
+
+function showMetaData(sampleID) {
+    // console.log(`showMetaData(${sampleID})`);
+    d3.json("data/samples.json").then(data => {
+        console.log(data)
+        var metaData = data.metadata;
+        var demoData = (metaData[0]);
+        console.log(demoData)
+        // var resultArray = samples.filter(s => s.id == sampleID);
+        // // console.log(resultArray);
+        // var result = resultArray[0];
+        // console.log(result);
+})
+}
+
+function optionChanged(newSampleID) {
     // console.log(`user selected ${newSampleID}`);
     drawBarGraph(newSampleID);
     drawBubbleChart(newSampleID);
@@ -92,12 +102,13 @@ function InitDashboard() {
                 .text(sampleID)
                 .property("value", sampleID);
         });
-    //create bargraph
+        //create bargraph
         var id = sampleNames[0];
         drawBarGraph(id);
         drawBubbleChart(id);
         showMetaData(id);
-    //create bubble chart
-    //update demographic info
-})}
+        //create bubble chart
+        //update demographic info
+    })
+}
 InitDashboard();
