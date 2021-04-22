edca9d2e686fdc0a6b8f026ce5dfe60540860a8c
Molly Sullivan <mchandler02@gmail.com>
2021-04-21 20:33:21 -0500

    added event handler and stubs for the functions

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,25 @@
 // console.log("app.js is loading");
 
 //Code based on instructor's code shared during office hours
+function drawBarGraph(sampleID){
+    console.log(`drawBarGraph(${sampleID})`);
+}
+
+function drawBubbleChart(sampleID){
+    console.log(`drawBubbleChart(${sampleID})`);
+}
+
+function showMetaData(sampleID){
+    console.log(`showMetaData(${sampleID})`);
+}
+
+function optionChanged(newSampleID){
+    console.log(`user selected ${newSampleID}`);
+    drawBarGraph(newSampleID);
+    drawBubbleChart(newSampleID);
+    showMetaData(newSampleID);
+}
+
 function InitDashboard() {
     // console.log("InitDashboard()");
 
@@ -10,14 +29,17 @@ function InitDashboard() {
     d3.json("data/samples.json").then(data => {
         // console.log(data)
         var sampleNames = data.names;
-        console.log(data.names)
+        // console.log(data.names)
         sampleNames.forEach(sampleID => {
             dropDown.append("option")
                 .text(sampleID)
                 .property("value", sampleID);
         });
-    // });
     //create bargraph
+        var id = sampleNames[0];
+        drawBarGraph(id);
+        drawBubbleChart(id);
+        showMetaData(id);
     //create bubble chart
     //update demographic info
 })}
