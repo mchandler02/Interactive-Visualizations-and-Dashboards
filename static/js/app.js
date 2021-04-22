07e7871de1ca1cf280ed8cdbd9d6ba57bf447d52
Molly Sullivan <mchandler02@gmail.com>
2021-04-21 20:23:30 -0500

    dropdown menu options added

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,24 @@
-console.log("app.js is loading");
+// console.log("app.js is loading");
 
+//Code based on instructor's code shared during office hours
 function InitDashboard() {
-    console.log("InitDashboard()");
+    // console.log("InitDashboard()");
 
     //populate dropdown
-    var dropdown = d3.select("#selDataset");
-    d3.json("samples.json").then(function(data) {
-        console.log(data)
-    })
+    var dropDown = d3.select("#selDataset");
+
+    d3.json("data/samples.json").then(data => {
+        // console.log(data)
+        var sampleNames = data.names;
+        console.log(data.names)
+        sampleNames.forEach(sampleID => {
+            dropDown.append("option")
+                .text(sampleID)
+                .property("value", sampleID);
+        });
+    // });
     //create bargraph
     //create bubble chart
     //update demographic info
-}
+})}
 InitDashboard();
